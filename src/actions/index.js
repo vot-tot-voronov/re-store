81211cd21636fc9949af253590cf465d498f7784
Ivan <frontenddeveloperweb@gmail.com>
2020-11-12 13:05:45 +0500

    Refactoring, added all functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
 const booksRequested = () => {
   return {
     type: 'FETCH_BOOKS_REQUEST'
-  }
+  };
 };
 
 const booksLoaded = (newBooks) => {
@@ -22,6 +22,20 @@ const booksError = (error) => {
 export const bookAddedToCart = (bookId) => {
   return {
     type: 'BOOK_ADDED_TO_CART',
+    payload: bookId
+  };
+};
+
+export const bookRemovedFromCart = (bookId) => {
+  return {
+    type: 'BOOK_REMOVED_FROM_CART',
+    payload: bookId
+  };
+};
+
+export const allBooksRemovedFromCart = (bookId) => {
+  return {
+    type: 'ALL_BOOKS_REMOVED_FROM_CART',
     payload: bookId
   };
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,86 +1,11 @@
+import updateBookList from './book-list';
+import updateShoppingCart from './shopping-cart';
 
-const initialState = {
-  books: [],
-  loading: true,
-  error: null,
-  cartItems: [],
-  orderTotal: 220
-};
-
-const updateCartItems = (cartItems, item, idx) => {
-
-  if (idx === -1) {
-    return [
-      ...cartItems,
-      item
-    ];
-  }
-
-  return [
-    ...cartItems.slice(0, idx),
-    item,
-    ...cartItems.slice(idx + 1)
-  ];
-};
-
-const updateCartItem = (book, item = {}) => {
-
-  const {
-    id = book.id,
-    count = 0,
-    title = book.title,
-    total = 0 } = item;
-
+const reducer = (state, action) => {
   return {
-    id,
-    title,
-    count: count + 1,
-    total: total + book.price
+    bookList: updateBookList(state, action),
+    shoppingCart: updateShoppingCart(state, action)
   };
-};
-
-const reducer = (state = initialState, action) => {
-
-  switch (action.type) {
-    case 'FETCH_BOOKS_REQUEST':
-      return {
-        ...state,
-        books: [],
-        loading: true,
-        error: null
-      };
-
-    case 'FETCH_BOOKS_SUCCESS':
-      return {
-        ...state,
-        books: action.payload,
-        loading: false,
-        error: null
-      };
-
-    case 'FETCH_BOOKS_FAILURE':
-      return {
-        ...state,
-        books: [],
-        loading: false,
-        error: action.payload
-      };
-
-    case 'BOOK_ADDED_TO_CART':
-      const bookId = action.payload;
-      const book = state.books.find((book) => book.id === bookId);
-      const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
-      const item = state.cartItems[itemIndex];
-
-      const newItem = updateCartItem(book, item);
-      return {
-        ...state,
-        cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
-      };
-
-    default:
-      return state;
-  }
 };
 
 export default reducer;
